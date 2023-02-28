36c49ce138e25c79a78b975f3a15ba02183af61e
Laercio Rios <laercio.rios13@hotmail.com>
2023-02-28 10:27:01 -0300

    :heavy_check_mark: Finish classwork 2

--- atividade2/Validate.py
+++ atividade2/Validate.py
@@ -1,7 +1,9 @@
 from lxml import etree
+
 parser = etree.XMLParser(dtd_validation=True)
 tree = etree.parse(open("Banco.xml", "rb"))
 dtd = etree.DTD(open("Banco.dtd", "rb"))
-print("Nome: <Coloque seu nome aqui>")
+
+print("Nome: Laercio de Souza Rios")
 print(dtd.validate(tree))
 print(dtd.error_log.filter_from_errors())
